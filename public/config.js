cb84d86f1bbfbf110a747b90ec0b748a335544e0
jaovic <j_victorfernandes@hotmail.com>
2022-03-16 16:36:05 -0300

    JS front-end

--- public/config.js
+++ public/config.js
@@ -1,24 +1,32 @@
-const { Console } = require("console");
+const form = document.getElementById("form");
+const imagem = document.getElementById("imagem");
+const btn = document.getElementById("btn");
 
-function searchRank() {
-  let steam32ID = document.getElementById("steam32ID").value;
-  fetch("localhost:3000/dota/ranking", {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST",
-    body: JSON.stringify({ steam32ID: steam32ID }),
-  })
-    .then((res) => {
-      Console.log(res);
-      return res.json();
-    })
-    .then((json) => {
-      if (json === Anciente) {
-        <img src="./" alt=""></img>;
-      }
+btn.addEventListener("click", function () {
+  console.log("teste");
+  // let steam32ID = document.getElementById("steam32ID").value;
+  // console.log(steam32ID);
+  // fetch("localhost:3000/dota/ranking", {
+  //   headers: {
+  //     Accept: "application/json",
+  //     "Content-Type": "application/json",
+  //   },
+  //   method: "POST",
+  //   body: JSON.stringify({ steam32ID: steam32ID }),
+  // })
+  //   .then((res) => {
+  //     Console.log(res);
+  //     return res.json();
+  //   })
+  //   .then((json) => {
+  //     const resultado = "";
+  //     if (json === Anciente) {
+  //       resultado = <img src="./imagens/ancient.png" alt="ancient Logo" />;
+  //     }
 
-      document.getElementById("imagem").innerHTML = postElements;
-    });
-}
+  //     document.getElementById("imagem").innerHTML = resultado;
+  form.style.display = "none";
+  imagem.style.display = "block";
+  console.log("teste");
+  // });
+});
